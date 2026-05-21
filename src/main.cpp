7d1d18576859a95e50aaeca24abaaf4168c92b5d
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-05-21 22:11:13 +0530

    Render chessboard using SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <SFML/Graphics.hpp>
-#include <optional>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({800u, 800u}), "Gambit");
+    const int WINDOW_SIZE = 800;
+    const int TILE_SIZE = WINDOW_SIZE / 8;
+
+    sf::RenderWindow window(
+        sf::VideoMode({WINDOW_SIZE, WINDOW_SIZE}),
+        "Gambit"
+    );
 
     while (window.isOpen())
     {
@@ -13,7 +18,35 @@ int main()
                 window.close();
         }
 
-        window.clear(sf::Color::Black);
+        window.clear();
+
+        // Draw chessboard
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                sf::RectangleShape tile(
+                    sf::Vector2f(
+                        static_cast<float>(TILE_SIZE),
+                        static_cast<float>(TILE_SIZE)
+                    )
+                );
+
+                tile.setPosition(
+                    {
+                        static_cast<float>(col * TILE_SIZE),
+                        static_cast<float>(row * TILE_SIZE)
+                    }
+                );
+
+                // Alternate colors
+                if ((row + col) % 2 == 0)
+                    tile.setFillColor(sf::Color(240, 217, 181)); // light
+                else
+                    tile.setFillColor(sf::Color(181, 136, 99));  // dark
+
+                window.draw(tile);
+            }
+        }
+
         window.display();
     }
 
